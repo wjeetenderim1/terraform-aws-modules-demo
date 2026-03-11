c36d74e1e6730e98dc74706616165dbb86447807
wjeetenderim1 <wjeetenderim1@gmail.com.com>
2026-03-11 20:26:29 +0530

    test

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,6 +2,6 @@ variable "subnet_id" {}
 variable "instance_type" {}
 variable "security_group_id" {}
 variable "instance_profile" {
-  description = "IAM instance profile for EC2"
+  description = "IAM instance profile for EC2."
   type        = string
 }
