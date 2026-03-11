1b0bb9d593198bd8a1c3eda9b2e2dc25a9ccb60d
wjeetenderim1 <wjeetenderim1@gmail.com.com>
2026-03-12 00:55:12 +0530

    1

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "alb_dns_name" {
   value = module.alb.alb_dns
 }
 
-output "rds_endpoint" {
-  value = module.aws_db_instance.rds.endpoint
-}
+# output "rds_endpoint" {
+#   value = module.aws_db_instance.rds.endpoint
+# }
